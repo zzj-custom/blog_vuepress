d612c33b63c97482c05daa2e96dd556d2d8a99c2
zqpzzj <1844066417@qq.com>
2022-03-04 22:17:08 +0800

    小问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   title: "风忆 blog",
-  description: '缓缓飘落的枫叶像思恋。',
+  description: '缓缓飘落的枫叶像思恋！！！',
   // base: '/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
